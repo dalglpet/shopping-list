0853d7b20b59c2f0bec4decdd8240a89f2512e67
Peter Dalgleish <peter.angus.d@gmail.com>
2025-09-07 18:00:22 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Main {
+	public static void main(String[] args) {
+		// Create a Shopping object
+	    Shopping shop = new Shopping();
+	    
+	    // Begin shopping loop
+	    shop.run();
+	}
+}
